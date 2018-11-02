022c0713b4fed3a593f67e9684fb74be7bbd6da9
unknown <cristianjulian132@hotmail.com>
2018-11-01 20:24:37 -0500

    Agrego suma

---
+++ src/ejemplogithub/Suma.java
@@ -0,0 +1,10 @@
+package ejemplogithub;
+
+public class Suma {
+
+    public static double suma(double sumando1, double sumando2) {
+        double resultado = sumando1 + sumando2;
+        return resultado;
+    }
+
+}
